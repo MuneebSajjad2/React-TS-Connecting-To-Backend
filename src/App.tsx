3c007ee3aa5f2868e6d409a441995a24709d30b1
Muneeb Sajjad <muneebsajjad2@gmail.com>
2023-08-03 00:05:29 +0500

    Updating  Data

--- src/App.tsx
+++ src/App.tsx
@@ -61,6 +61,23 @@ const App = () => {
 
     }
 
+    const updateUser = (user : User) => {
+      const orignalUsers = [...users];
+      const updatedUser = {...user, name : user.name + '!'}
+
+      setUsers(users.map(u => u.id === user.id ? updatedUser : u))
+
+
+      axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id , updatedUser)
+      .catch((err) => {
+        setError(err.message)
+        setUsers(orignalUsers)
+
+      })
+
+
+    }
+
   return (
     <div>
       {error && <p>{error}</p>}
@@ -68,7 +85,10 @@ const App = () => {
       <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
       <ul className='list-group'>
         {users.map(user=>  <li key={user.id} className='list-group-item d-flex justify-content-between'>{user.name}
+        <div >
+        <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
         <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+        </div>
         </li>)}
       </ul>
     </div>
